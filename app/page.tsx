ad40829d265892ccc6e76a4ab8f2e7a8e24682de
bolognini <matheusbolognini79@gmail.com>
2024-10-25 16:00:02 +0200

    replicate test test test

--- app/page.tsx
+++ app/page.tsx
@@ -2,22 +2,23 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div className="video-background">
-    <video autoPlay muted loop>
-      <source src="background.mp4" type="video/mp4" />
-    </video>
-    
-  </div>
-    <div className="container">
-    <p className="glitch">
-      <span aria-hidden="true">The Test Library</span>
-      The Test Library
-      <span aria-hidden="true">The Test Library</span>
-    </p>
-      <p>Welcome Tester</p>
-      <p>Search archived_reports and data</p>
-      <p>Use the related Report Number ID</p>
-      <p>https://276089230.pages.dev/(Report Number ID)</p>
+        <video autoPlay muted loop>
+          <source src="https://i.imgur.com/ySiFZHm.mp4" type="video/mp4" />
+        </video>
+      </div>
+      <div className="container">
+        <p className="glitch">
+          <span aria-hidden="true">The Test Library</span>
+          The Test Library
+          <span aria-hidden="true">The Test Library</span>
+        </p>
+        <p>Welcome Tester</p>
+        <p>Search archived_reports and data</p>
+        <p>Use the related Report Number ID</p>
+        <p>
+          https://nextjs-boilerplate-green-five-84.vercel.app/(Report Number ID)
+        </p>
+      </div>
     </div>
-    </div>
-  );
+  )
 }
---
+++ pages/113954914091772734.tsx
@@ -0,0 +1,22 @@
+import '../app/globals.css'
+
+export default function Third() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div className="video-background">
+        <video autoPlay muted loop>
+          <source src="https://i.imgur.com/ySiFZHm.mp4" type="video/mp4" />
+        </video>
+      </div>
+      <div className="container">
+        <b>REPORT REPORT REPORT</b>
+        <p>quick brown fox jumps </p>
+        <p>13 16 f9 cc 39 3f 1f 63 61 c9 ff 63 69 fc 16 c6 c3 f6</p>
+        <p>over the lazy dog</p>
+        <p>63 9c 19 1f 1c c1 19 36 f3 91 9f 6c 63 6f</p>
+        <p>0123456789</p>
+        <p>96 11 cf 33 31 93 66 f1 3c 99 </p>
+      </div>
+    </div>
+  )
+}
